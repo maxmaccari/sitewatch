3fbef0d10341eaeeaa21623798d3922f1cd6785a
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-08-09 01:25:50 -0400

    Implement e2e tests

--- tests/e2e/specs/ping-website.js
+++ tests/e2e/specs/ping-website.js
@@ -1,6 +1,68 @@
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('Ping website', () => {
+  it('Ping website with correct url if latency is good', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: '*',
+      response: [],
+    }).as('pingSite')
+
     cy.visit('/')
-    cy.contains('h1', 'How fast is your website loading?')
+    cy.get('[data-test-id="site-input"]').type('www.example.com')
+    cy.get('[data-test-id="ping-button"]').click()
+
+    cy.wait('@pingSite')
+
+    cy.contains('www.example.com')
+    cy.contains('The latency of www.example.com is good.')
+
+    cy.contains('[data-test-id="ping-button"]', 'Retry')
+
+    cy.get('[data-test-id="site-input"]').type('/www')
+    cy.contains('[data-test-id="ping-button"]', 'Ping')
+  })
+
+  it('Ping website with correct url if latency is average', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: '*',
+      response: [],
+      delay: 500,
+    }).as('pingSite')
+
+    cy.visit('/')
+    cy.get('[data-test-id="site-input"]').type('www.example.com')
+    cy.get('[data-test-id="ping-button"]').click()
+
+    cy.wait('@pingSite')
+
+    cy.contains('www.example.com')
+    cy.contains('The latency of www.example.com is average.')
+  })
+
+  it('Ping website with correct url if latency is bad', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: '*',
+      response: [],
+      delay: 1200,
+    }).as('pingSite')
+
+    cy.visit('/')
+    cy.get('[data-test-id="site-input"]').type('www.example.com')
+    cy.get('[data-test-id="ping-button"]').click()
+
+    cy.wait('@pingSite')
+
+    cy.contains('www.example.com')
+    cy.contains('The latency of www.example.com is bad.')
+  })
+
+  it('Ping website with invalid url', () => {
+    cy.visit('/')
+    cy.get('[data-test-id="site-input"]').type('invalid url')
+    cy.get('[data-test-id="ping-button"]').should('be.disabled')
   })
 })
