0d773419a4fd55c1405ae98affadc1e99ade0966
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-08-09 08:21:16 -0400

    Implement history actions

--- src/store/__tests__/actions.spec.js
+++ src/store/__tests__/actions.spec.js
@@ -4,7 +4,7 @@ import BrowserPingService from '@/services/BrowserPingService'
 jest.mock('@/services/BrowserPingService')
 
 describe('pingSite', () => {
-  it('should call BrowserPingService.ping with the given url', async () => {
+  it('calls BrowserPingService.ping with the given url', async () => {
     BrowserPingService.ping.mockResolvedValue(100)
     const commit = jest.fn()
     const siteUrl = 'http://www.example.com'
@@ -15,7 +15,7 @@ describe('pingSite', () => {
     expect(BrowserPingService.ping).toBeCalledWith(siteUrl)
   })
 
-  it('should commit START_LOADING and SET_PING_RESULT with the given url and the timeout if ping is sucessfull', async () => {
+  it('commits START_LOADING and SET_PING_RESULT with the given url and the timeout if ping is sucessfull', async () => {
     const latency = 250
     const siteUrl = 'http://www.example.com'
     const commit = jest.fn()
@@ -29,7 +29,7 @@ describe('pingSite', () => {
     expect(commit).toBeCalledWith('START_LOADING')
   })
 
-  it('should commit START_LOADING and SET_ERROR with the given url and the error in case of errors', async () => {
+  it('commits START_LOADING and SET_ERROR with the given url and the error in case of errors', async () => {
     const error = 'my error'
     const siteUrl = 'http://www.example.com'
     const commit = jest.fn()
@@ -41,5 +41,84 @@ describe('pingSite', () => {
     expect(commit).toBeCalledTimes(2)
     expect(commit).toBeCalledWith('SET_ERROR', { error, siteUrl })
     expect(commit).toBeCalledWith('START_LOADING')
+  })
+})
+
+describe('goToPage', () => {
+  it('commits SET_CURRENT_PAGE with the given page', () => {
+    const commit = jest.fn()
+    const page = 342
+
+    actions.goToPage({ commit }, page)
+
+    expect(commit).toBeCalledTimes(1)
+    expect(commit).toBeCalledWith('SET_CURRENT_PAGE', page)
+  })
+})
+
+describe('nextPage', () => {
+  it('commits SET_CURRENT_PAGE with next page if exists', () => {
+    const commit = jest.fn()
+    const state = { currentPage: 1 }
+    const getters = { historyPages: 2 }
+
+    actions.nextPage({ commit, state, getters })
+
+    expect(commit).toBeCalledTimes(1)
+    expect(commit).toBeCalledWith('SET_CURRENT_PAGE', 2)
+  })
+
+  it('should not commit SET_CURRENT_PAGE with next page if it not exists', () => {
+    const commit = jest.fn()
+    const state = { currentPage: 1 }
+    const getters = { historyPages: 1 }
+
+    actions.nextPage({ commit, state, getters })
+
+    expect(commit).toBeCalledTimes(0)
+  })
+})
+
+describe('previousPage', () => {
+  it('commits SET_CURRENT_PAGE with previous page if exists', () => {
+    const commit = jest.fn()
+    const state = { currentPage: 2 }
+
+    actions.previousPage({ commit, state })
+
+    expect(commit).toBeCalledTimes(1)
+    expect(commit).toBeCalledWith('SET_CURRENT_PAGE', 1)
+  })
+
+  it('should not commit SET_CURRENT_PAGE with previous page if it not exists', () => {
+    const commit = jest.fn()
+    const state = { currentPage: 1 }
+
+    actions.previousPage({ commit, state })
+
+    expect(commit).toBeCalledTimes(0)
+  })
+})
+
+describe('searchHistory', () => {
+  it('commits SET_HISTORY_SEARCH with the given search', () => {
+    const commit = jest.fn()
+    const search = 'my search'
+
+    actions.searchHistory({ commit }, search)
+
+    expect(commit).toBeCalledTimes(1)
+    expect(commit).toBeCalledWith('SET_HISTORY_SEARCH', search)
+  })
+})
+
+describe('resetHistory', () => {
+  it('commits RESET_HISTORY', () => {
+    const commit = jest.fn()
+
+    actions.resetHistory({ commit })
+
+    expect(commit).toBeCalledTimes(1)
+    expect(commit).toBeCalledWith('RESET_HISTORY')
   })
 })
