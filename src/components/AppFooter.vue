31940578ad4957d4023fab577700d6c45ea85155
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-08-14 18:19:23 -0400

    Implement footer styles

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="app-footer">
+  <div
+    class="app-footer w-full text-center bg-gray-800 text-gray-100 px-2 py-4"
+  >
     <p>
       Developed by Maxsuel Maccari -
-      <a href="https://www.github.com/maxmaccari" target="_blank">
+      <a
+        class="hover:text-white underline transition-colors duration-500"
+        href="https://www.github.com/maxmaccari"
+        target="_blank"
+      >
         https://www.github.com/maxmaccari
       </a>
     </p>
