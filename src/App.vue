f52a89aba20aff4c6cc2c8441ea26c2493eec8c0
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-08-14 15:12:57 -0400

    Adjust App styles

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app">
-    <div class="container top-section">
-      <h1>How fast is your website loading?</h1>
-      <p>
+  <div id="app" class="antialiased">
+    <div class="px-2">
+      <h1 class="mt-6 text-3xl font-bold text-center leading-tight">
+        How fast is your website loading?
+      </h1>
+      <p class="mt-4 text-xl text-gray-700 text-center leading-tight">
         Enter a url bellow to measure the website latency and we will show how
         fast (or slow) it is.
       </p>
 
-      <PingSection />
+      <PingSection class="mt-4" />
     </div>
 
-    <HistorySection />
+    <HistorySection class="mt-4" />
 
     <AppFooter />
   </div>
