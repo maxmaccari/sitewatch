84805955799c935d1e311e043c508ab37b85f44f
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-08-24 19:55:31 -0400

    Improve the way that errors are returned

--- src/services/__tests__/PingService.spec.js
+++ src/services/__tests__/PingService.spec.js
@@ -36,12 +36,41 @@ describe('PingService', () => {
       )
     })
 
+    it('should return error if api returns an error', async () => {
+      const error = {
+        response: {
+          data: {
+            url: 'http://www.example.com',
+            code: 'ENOTFOUND',
+            message: 'getaddrinfo ENOTFOUND www.example.com',
+          },
+        },
+      }
+      Http.post.mockRejectedValue(error)
+
+      await expect(PingService.ping('http://www.example.com')).rejects.toEqual(
+        error.response.data
+      )
+    })
+
+    it('should return error if url is invalid', async () => {
+      jest.spyOn(navigator, 'onLine', 'get').mockReturnValueOnce(false)
+
+      await expect(PingService.ping('invalid url')).rejects.toEqual({
+        code: 'invalid_url',
+        url: 'invalid url',
+      })
+
+      expect(Http.post).toHaveBeenCalledTimes(0)
+    })
+
     it('should return error if browser is offline', async () => {
       jest.spyOn(navigator, 'onLine', 'get').mockReturnValueOnce(false)
 
-      await expect(PingService.ping('http://www.example.com')).rejects.toEqual(
-        'network_error'
-      )
+      await expect(PingService.ping('http://www.example.com')).rejects.toEqual({
+        code: 'network_error',
+        url: 'http://www.example.com',
+      })
 
       expect(Http.post).toHaveBeenCalledTimes(0)
     })
