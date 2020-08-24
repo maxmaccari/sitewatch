079dfbb3e82760a675d0c21a79f1c2835f3c3402
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-08-24 19:18:46 -0400

    Fiz pingHistory loading

--- src/store/plugins/persistedStatePlugin.js
+++ src/store/plugins/persistedStatePlugin.js
@@ -1,8 +1,25 @@
 import createPersistedState from 'vuex-persistedstate'
+import PingResult from '@/models/PingResult'
 
 export const PERSISTED_STATE_KEY = 'sitewatcher'
 
 export default createPersistedState({
   key: PERSISTED_STATE_KEY,
   paths: ['pingHistory'],
+  getState(key, storage) {
+    const json = storage.getItem(key)
+
+    try {
+      if (!json) return undefined
+      let { pingHistory } = JSON.parse(json)
+
+      return {
+        pingHistory: pingHistory.map(
+          ({ _params: params }) => new PingResult(params)
+        ),
+      }
+    } catch (err) {
+      return undefined
+    }
+  },
 })
